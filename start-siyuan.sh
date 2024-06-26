7a46ff7a0930db1e0cab92da6a1a84d98ba6923a
5l2 <48936628+5l2@users.noreply.github.com>
2024-06-26 19:36:46 +0800

    Increase the compatibility of Siyuan by checking the current environment.

--- start-siyuan.sh
+++ start-siyuan.sh
@@ -1,5 +1,33 @@
-#!/bin/sh
+#!/bin/bash
+
 set -oue pipefail
+
+EXTRA_ARGS=(--enable-features=Vulkan)
+
+# Wayland support can be optionally enabled like so:
+# flatpak override --user --socket=wayland org.b3log.siyuan
+
+WL_DISPLAY="${WAYLAND_DISPLAY:-"wayland-0"}"
+# Some compositors a real path a instead of a symlink for WAYLAND_DISPLAY:
+# https://github.com/flathub/md.obsidian.Obsidian/issues/284
+if [[ -e "${XDG_RUNTIME_DIR}/${WL_DISPLAY}" || -e "/${WL_DISPLAY}" ]]; then
+    echo "Debug: Enabling Wayland backend"
+    EXTRA_ARGS+=(
+        --ozone-platform-hint=auto
+        --enable-features=UseOzonePlatform,WaylandWindowDecorations
+    )
+    if [[ -c /dev/nvidia0 ]]; then
+        echo "Debug: Detecting Nvidia GPU. disabling GPU sandbox."
+        EXTRA_ARGS+=(
+            --disable-gpu-sandbox
+        )
+    fi
+fi
+
+echo "Debug: Will run Siyuan with the following arguments: ${EXTRA_ARGS[@]}"
+echo "Debug: Additionally, user gave: $@"
+
 export FLATPAK_ID="${FLATPAK_ID:-org.b3log.siyuan}"
 export TMPDIR="${XDG_RUNTIME_DIR}/app/${FLATPAK_ID}"
-zypak-wrapper /app/siyuan/siyuan --ozone-platform-hint=auto --enable-features=UseOzonePlatform,WaylandWindowDecorations,Vulkan --gtk-version=4 $@
+
+zypak-wrapper /app/siyuan/siyuan $@ ${EXTRA_ARGS[@]}
